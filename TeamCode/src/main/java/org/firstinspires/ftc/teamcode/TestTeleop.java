521d13a57ed4b5a54ea40ad15d08dad52edbefd0
Lily Cox <lilymcox@gmail.com>
2021-11-16 18:53:05 -0800

    Added TestTeleop from previous year

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleop.java
@@ -0,0 +1,60 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name="Linear_OpMode", group="Teleop")
+public class TestTeleop extends LinearOpMode {
+
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor FleftDrive = null;
+    private DcMotor FrightDrive = null;
+    private DcMotor BleftDrive = null;
+    private DcMotor BrightDrive = null;
+
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+
+        FleftDrive = hardwareMap.get(DcMotor.class, "FleftDrive");
+        FrightDrive = hardwareMap.get(DcMotor.class, "FrightDrive");
+        BleftDrive = hardwareMap.get(DcMotor.class, "BleftDrive");
+        BrightDrive = hardwareMap.get(DcMotor.class, "BrightDrive");
+
+
+        FleftDrive.setDirection(DcMotor.Direction.FORWARD);
+        FrightDrive.setDirection(DcMotor.Direction.REVERSE);
+
+        BleftDrive.setDirection(DcMotor.Direction.FORWARD);
+        BrightDrive.setDirection(DcMotor.Direction.REVERSE);
+
+        waitForStart();
+        runtime.reset();
+
+        while (opModeIsActive()) {
+
+            double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
+            double robotAngle = Math.atan2(gamepad1.left_stick_y, -gamepad1.left_stick_x) - Math.PI / 4;
+            double rightX = gamepad1.right_stick_x;
+            final double v1 = r * Math.cos(robotAngle) - rightX;
+            final double v2 = r * Math.sin(robotAngle) + rightX;
+            final double v3 = r * Math.sin(robotAngle) - rightX;
+            final double v4 = r * Math.cos(robotAngle) + rightX;
+
+            FleftDrive.setPower(v1);
+            FrightDrive.setPower(v2);
+            BleftDrive.setPower(v3);
+            BrightDrive.setPower(v4);
+        }
+
+    }
+}
+
