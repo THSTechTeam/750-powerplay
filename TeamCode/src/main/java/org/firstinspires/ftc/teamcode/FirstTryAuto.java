fffe0dc9da41ccdedb152c79417c02e9ede41e68
Jordan White <jordan.alys.456@gmail.com>
2021-12-07 19:30:16 -0800

    t

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstTryAuto.java
@@ -0,0 +1,147 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;//
+
+
+//first attempt at actual autonomous mode
+
+@Autonomous(name="Auto 11/30", group="Auto")
+public class FirstTryAuto extends LinearOpMode {
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor frontLeft = null;
+    private DcMotor frontRight = null;
+    private DcMotor rearLeft = null;
+    private DcMotor rearRight = null;
+    private DcMotor carouselWheel = null;
+
+    public void DriveForward(double power){
+        frontLeft.setPower(power);
+        frontRight.setPower(power);
+        rearLeft.setPower(power);
+        rearRight.setPower(power);
+    }
+
+    public void stopRobot()
+    {
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        rearLeft.setPower(0);
+        rearRight.setPower(0);
+    }
+
+    public void turnLeft(double power)
+    {
+        frontLeft.setPower(power);
+        frontRight.setPower(-power);
+        rearLeft.setPower(power);
+        rearRight.setPower(-power);
+    }
+
+    public void turnRight(double power)
+    {
+        turnLeft(-power);
+    }
+
+    public void turnAround(){
+        turnRight(.5);
+        sleep(1000);
+    }
+
+    public void carousel(double power){
+        carouselWheel.setPower(power);
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry.addData("Status", "Initialized");
+
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        rearLeft = hardwareMap.get(DcMotor.class, "rearLeft");
+        rearRight = hardwareMap.get(DcMotor.class, "rearRight");
+        carouselWheel = hardwareMap.get(DcMotor.class, "carouselWheel");
+
+
+        // Making a shot in the dark guess that the right ones need to be reeversed.
+        frontLeft.setDirection(DcMotor.Direction.FORWARD);
+        rearLeft.setDirection(DcMotor.Direction.FORWARD);
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        rearRight.setDirection(DcMotor.Direction.REVERSE);
+        carouselWheel.setDirection(DcMotor.Direction.FORWARD);
+
+        // Tell the driver that initialization is complete.
+        telemetry.addData("Status", "Initialized");
+
+        waitForStart();
+
+        DriveForward(.75);
+        sleep(1190);
+        turnRight(.5);
+        sleep(480);
+        DriveForward(.5);
+        sleep(270);
+        stopRobot();
+        sleep(4000);
+        turnAround();
+        turnRight(.5);
+        sleep(250);
+        DriveForward(1);
+        sleep(800);
+        turnLeft(.5);
+        sleep(500);
+        DriveForward(.8);
+        sleep(1000);
+        carousel(1);
+        sleep(1000);
+
+       /* turnRight(.6);
+        sleep(755);
+        DriveForward(1);
+        sleep(1900);
+        /*turnLeft(.5);
+        sleep(1000);
+        turnRight(.5);
+        sleep(250);
+*/
+
+    }
+}
