fcac11d6472fc6e18298c37c5d630a2982059cc0
Jordan White <jordan.alys.456@gmail.com>
2021-12-09 18:54:08 -0800

    Retrying red carousel
    Red carousel as of 12/9

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptTensorFlowObjectDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptTensorFlowObjectDetection.java
@@ -1,42 +1,8 @@
-/* Copyright (c) 2019 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-<<<<<<< HEAD
 package org.firstinspires.ftc.teamcode;
-=======
-package org.firstinspires.ftc.robotcontroller.external.samples;
->>>>>>> 797a4a41fcb6bb54bdd495c569dae64ac7cd000a
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-<<<<<<< HEAD
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
@@ -45,14 +11,14 @@ import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
 
 import java.util.List;
-=======
+
 import java.util.List;
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection;
 import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
 import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
->>>>>>> 797a4a41fcb6bb54bdd495c569dae64ac7cd000a
+
 
 /**
  * This 2020-2021 OpMode illustrates the basics of using the TensorFlow Object Detection API to
@@ -65,11 +31,10 @@ import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
  * is explained below.
  */
 @TeleOp(name = "Concept: TensorFlow Object Detection", group = "Concept")
-<<<<<<< HEAD
 @Disabled
-=======
+
 //@Disabled
->>>>>>> 797a4a41fcb6bb54bdd495c569dae64ac7cd000a
+
 public class ConceptTensorFlowObjectDetection extends LinearOpMode {
   /* Note: This sample uses the all-objects Tensor Flow model (FreightFrenzy_BCDM.tflite), which contains
    * the following 4 detectable objects
@@ -103,11 +68,8 @@ public class ConceptTensorFlowObjectDetection extends LinearOpMode {
      * and paste it in to your code on the next line, between the double quotes.
      */
     private static final String VUFORIA_KEY =
-<<<<<<< HEAD
-            " -- YOUR NEW VUFORIA KEY GOES HERE  --- ";
-=======
             "AboYhDH/////AAABmQsjktyIQk0yh9uC4mJ2ZIVF9koH50+zKA1iIRrZeTxVGLcGtHMtlOTOox7FlE7SR0sdjjcP2tJK0fmQ6uPhTX3GyA2pvFd2utrSH2rs5pJz59sCGhDz2UMHNcIuacEjvYfgHArYxTbcLGZBEk9Y1YQT6wA3c/IR+INEt/TvSqvVusdBpfzpnx8NOCe3MtliEpH2+Z5wID1URumjt7XR5lzV8YqQOXadbF7Bq+4Gb1DNWHBAY3OrLNfzLVx4Ys5Tf1QFyeHVhuxUb3YiO1FIXyrZQpYrT2hx4NbCADebV1+iHU0PbImOQZSSDqVHSbDM6ygYyfwtWHYDZTzLnoKCveVa8NIt581hE2jrurbecj7/";
->>>>>>> 797a4a41fcb6bb54bdd495c569dae64ac7cd000a
+
 
     /**
      * {@link #vuforia} is the variable we will use to store our instance of the Vuforia
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedCaroRetry.java
@@ -0,0 +1,113 @@
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+//first attempt at actual autonomous mode
+
+@Autonomous(name="Auto RedCheck", group="Auto")
+public class RedCaroRetry extends LinearOpMode {
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor frontLeft = null;
+    private DcMotor frontRight = null;
+    private DcMotor rearLeft = null;
+    private DcMotor rearRight = null;
+    private DcMotor carouselWheel = null;
+
+    public void DriveForward(double power){
+        frontLeft.setPower(power);
+        frontRight.setPower(power);
+        rearLeft.setPower(power);
+        rearRight.setPower(power);
+    }
+
+    public void stopRobot()
+    {
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        rearLeft.setPower(0);
+        rearRight.setPower(0);
+    }
+
+    public void turnLeft(double power)
+    {
+        frontLeft.setPower(power);
+        frontRight.setPower(-power);
+        rearLeft.setPower(power);
+        rearRight.setPower(-power);
+    }
+
+    public void turnRight(double power)
+    {
+        turnLeft(-power);
+    }
+
+    public void turnAround(){
+        turnRight(.5);
+        sleep(1000);
+    }
+
+    public void carousel(double power){
+        carouselWheel.setPower(power);
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry.addData("Status", "Initialized");
+
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        rearLeft = hardwareMap.get(DcMotor.class, "rearLeft");
+        rearRight = hardwareMap.get(DcMotor.class, "rearRight");
+        carouselWheel = hardwareMap.get(DcMotor.class, "carouselWheel");
+
+
+        // Making a shot in the dark guess that the right ones need to be reeversed.
+        frontLeft.setDirection(DcMotor.Direction.FORWARD);
+        rearLeft.setDirection(DcMotor.Direction.FORWARD);
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        rearRight.setDirection(DcMotor.Direction.REVERSE);
+        carouselWheel.setDirection(DcMotor.Direction.FORWARD);
+
+        // Tell the driver that initialization is complete.
+        telemetry.addData("Status", "Initialized");
+
+        waitForStart();
+
+        DriveForward(.85);
+        sleep(800);
+        turnLeft(.3);
+        sleep(700);
+        DriveForward(1);
+        sleep(460);
+        turnLeft(.21);
+        sleep(1225);
+        DriveForward(.35);
+        sleep(2490);
+        stopRobot();
+        carouselWheel.setPower(1);
+        sleep(2750);
+        DriveForward(-80);
+        sleep(350);
+
+       /* turnRight(.6);
+        sleep(755);
+        DriveForward(1);
+        sleep(1900);
+        /*turnLeft(.5);
+        sleep(1000);
+        turnRight(.5);
+        sleep(250);
+*/
+
+    }
+}
