36afd1ed9e3e8c009427f03173e23f5e5ef6a282
Brandon <pacewicbrandon@gmail.com>
2023-01-04 10:18:06 -0800

    Added lift debugger and fixed lift

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiftDebugger.java
@@ -0,0 +1,57 @@
+package org.firtsinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Config
+@TeleOp(name="Lift Debugger", group="TeleOp")
+public class LiftDebugger extends LinearOpMode {
+    private DcMotor motorLift = null;
+
+    public static int LIFT_LEVEL_0 = -1075;
+    public static int LIFT_LEVEL_1 = 450;
+    public static int LIFT_LEVEL_2 = 1300;
+    public static int LIFT_LEVEL_3 = 2300;
+    public static double LIFT_SPEED = 0.5;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        motorLift = hardwareMap.get(DcMotor.class, "motorLift");
+
+        while (!isStopRequested() && !isStarted()) {
+            telemetry.addData("Lift Level 0", LIFT_LEVEL_0);
+            telemetry.addData("Lift Level 1", LIFT_LEVEL_1);
+            telemetry.addData("Lift Level 2", LIFT_LEVEL_2);
+            telemetry.addData("Lift Level 3", LIFT_LEVEL_3);
+            telemetry.addData("Lift Speed", LIFT_SPEED);
+            telemetry.addData("Current Lift Level", motorLift.getCurrentPosition());
+            telemetry.update();
+        }
+
+        while (opModeIsActive()) {
+            if (gamepad2.x) {
+                motorLift.setTargetPosition(LIFT_LEVEL_1);
+                motorLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                motorLift.setPower(LIFT_SPEED);
+            } else if(gamepad2.y) {
+                motorLift.setTargetPosition(LIFT_LEVEL_2);
+                motorLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                motorLift.setPower(LIFT_SPEED);
+            } else if(gamepad2.b) {
+                motorLift.setTargetPosition(LIFT_LEVEL_3);
+                motorLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                motorLift.setPower(LIFT_SPEED);
+            } else if(gamepad2.a) {
+                motorLift.setTargetPosition(LIFT_LEVEL_0);
+                motorLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                motorLift.setPower(LIFT_SPEED);
+            }
+
+            telemetry.addData("Current Lift Level", motorLift.getCurrentPosition());
+            telemetry.update();
+            idle();
+        }
+    }
+}
