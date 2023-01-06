b2ca9c38e582f419b8eb2d51a4cdd5cbc7b75898
Brandon P <pacewicbrandon@gmail.com>
2023-01-05 18:29:35 -0800

    Added PID Controller (#5)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricMecanumDrive.java
@@ -4,27 +4,34 @@ import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import org.firstinspires.ftc.teamcode.util.MecanumDriveManager;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import org.firstinspires.ftc.teamcode.util.DriveMode;
+import org.firstinspires.ftc.teamcode.util.MecanumDriveManager;
+import org.firstinspires.ftc.teamcode.util.PIDController;
 
 @Config
 @TeleOp(name="Field Centric Mecanum Drive", group="TeleOp")
 public class FieldCentricMecanumDrive extends LinearOpMode {
     private MecanumDriveManager drive;
 
-    private DcMotor motorLift = null;
     private CRServo servoGrabber = null;
     private CRServo servoPivot = null;
 
     /** Change these values to modify motor/servo positions and speeds ****************************/
+    private PIDController liftController;
+    public static double LIFT_KP = 0.0008;
+    public static double LIFT_KI = 0.0005;
+    public static double LIFT_KD = 0.00025;
 
+    public static int MANUAL_LIFT_INCREMENT = 50;
+    
     // These values are marked as public to allow the dashboard to display them for tuning. 
     public static int LIFT_LEVEL_0 = 100;
     public static int LIFT_LEVEL_1 = 1500;
     public static int LIFT_LEVEL_2 = 2400;
     public static int LIFT_LEVEL_3 = 3400;
-    private static final double LIFT_SPEED = 1;
+    public static double LIFT_POWER = 1;
 
     private static final double PIVOT_POWER = 0.7;
     private static final double PIVOT_FRONT_POSITION = 1;
@@ -34,13 +41,13 @@ public class FieldCentricMecanumDrive extends LinearOpMode {
 
     /**********************************************************************************************/
 
-    private final double lowPowerFactor = 0.3;
-    private final double highPowerFactor = 0.75;
+    public static double lowPowerFactor = 0.4;
+    public static double highPowerFactor = 0.6;
 
     private int currentLiftLevel = 0;
     private double motorPowerFactor = lowPowerFactor;
 
-    private double getPowerFactor(final double previousPowerFactor) {
+    private double getPowerFactor(double previousPowerFactor) {
        if (gamepad1.a) {
            return lowPowerFactor;
        } else if (gamepad1.b) {
@@ -52,7 +59,16 @@ public class FieldCentricMecanumDrive extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
-        motorLift = hardwareMap.dcMotor.get("motorLift");
+        liftController = new PIDController(
+                LIFT_KP, 
+                LIFT_KI, 
+                LIFT_KD,
+                hardwareMap.get(DcMotorEx.class, "motorLift"),
+                DcMotorSimple.Direction.FORWARD
+            );
+        liftController.setEncoderConstraints(0, 3700);
+        liftController.setMaxMotorPower(LIFT_POWER);
+
         servoPivot = hardwareMap.get(CRServo.class, "servoPivot");
         servoGrabber = hardwareMap.get(CRServo.class, "servoGrabber");
 
@@ -70,37 +86,33 @@ public class FieldCentricMecanumDrive extends LinearOpMode {
         }
 
         while (opModeIsActive()) {
-            telemetry.addData("Currently at", " at %7d", motorLift.getCurrentPosition());
+            telemetry.addData("Currently at", " at %7d", liftController.getCurrentPosition());
             telemetry.update();
+            liftController.update();
 
             /** Lift Code *************************************************************************/
 
-            if(gamepad2.x){
-                motorLift.setTargetPosition(LIFT_LEVEL_1);
-                motorLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                motorLift.setPower(LIFT_SPEED);
+            if (gamepad2.x) {
+                liftController.setTargetPosition(LIFT_LEVEL_1);
             }
-            else if(gamepad2.y){
-                motorLift.setTargetPosition(LIFT_LEVEL_2);
-                motorLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                motorLift.setPower(LIFT_SPEED);
+            else if (gamepad2.y) {
+                liftController.setTargetPosition(LIFT_LEVEL_2);
             }
-            else if(gamepad2.b){
-                motorLift.setTargetPosition(LIFT_LEVEL_3);
-                motorLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                motorLift.setPower(LIFT_SPEED);
+            else if (gamepad2.b) {
+                liftController.setTargetPosition(LIFT_LEVEL_3);
             }
-            else if(gamepad2.a){
-                motorLift.setTargetPosition(LIFT_LEVEL_0);
-                motorLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                motorLift.setPower(LIFT_SPEED);
+            else if (gamepad2.a) {
+                liftController.setTargetPosition(LIFT_LEVEL_0);
             }
 
-            if(gamepad2.dpad_up && !gamepad2.dpad_down) {
-                motorLift.setPower(1);
+            if (gamepad2.dpad_up) {
+                liftController.setTargetPosition(liftController.getTargetPosition() + MANUAL_LIFT_INCREMENT);
+            } else if (gamepad2.dpad_down) {
+                liftController.setTargetPosition(liftController.getTargetPosition() - MANUAL_LIFT_INCREMENT);
             }
-            else if(gamepad2.dpad_down && !gamepad2.dpad_up) {
-                motorLift.setPower(0);
+
+            if (gamepad2.dpad_right) {
+                liftController.resetEncoder();
             }
 
             /** Pivot Code ************************************************************************/
